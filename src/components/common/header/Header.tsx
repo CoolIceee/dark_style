dae33bfeaf7e18e946aff997a0e13aeedfe1646f
Магомед Магомадов <magomedmagomadov@192.168.2.10>
2023-02-11 17:45:34 +0300

    pin header on scroll

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -9,7 +9,7 @@ export const Header: React.FC = () => {
   return (
     <>
       <div className='h-20 max-w-full object-cover rounded-b-3xl' style={{ backgroundImage: `url(${bgImg as string})` }}></div>
-      <div className='flex h-auto px-9 max-w-full border-b-2 border-blue-100 '>
+      <div className='flex h-auto px-9 max-w-full sticky top-0 bg-white'>
         <nav className='justify-between w-50 mx-auto self-center  h-full sm:justify-between xl:flex xl:w-full  xl:h-20 xl:mx-0 sm:mx-0 sm:w-full sm:flex sm:h-20'>
           <ul className='flex w-[70%] h-full pt-2 mx-auto  xl:w-[35%]'>
             <li className='px-2 flex self-center h-12  mx-auto xl:w-[35%] xl:mx-0 sm:mx-0'>
@@ -21,7 +21,7 @@ export const Header: React.FC = () => {
               <Search />
             </li>
           </ul>
-          <ul className='flex absolute bottom-0 left-0 px-10 justify-between h-20 w-full xl:flex-row xl:p-0 xl:self-center xl:static xl:justify-end xl:w-[35%] xl:h-full sm:absolute sm:w-20 sm:flex-col sm:bottom-24 sm:h-auto sm:px-6'>
+          <ul className='flex fixed bottom-0 left-0 px-10 justify-between h-20 w-full xl:flex-row xl:p-0 xl:self-center xl:static xl:justify-end xl:w-[35%] xl:h-full sm:fixed sm:w-20 sm:flex-col sm:bottom-24 sm:h-auto sm:px-6'>
             <li className='px-2 flex self-center w-12 sm:py-2'>
               <Profile />
             </li>
